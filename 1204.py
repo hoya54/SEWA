bfaceda5c726f6dcce31111d8867244b3bc9bcc2
hoya54 <hoya9654@gmail.com>
2022-05-03 17:55:10 +0900

    220503

--- 1204.py
+++ 1204.py
@@ -1,2 +1,21 @@
 T = int(input())
 
+for j in range(T):
+    s = int(input())
+
+    scores = [0]*101
+
+    lst = list(map(int, input().split()))
+
+    for i in lst:
+        scores[i] += 1
+
+    maxx = max(scores)
+
+    result = []
+    for i in range(0, 101):
+        if scores[i] == maxx:
+            result.append(i)
+    
+    print("#{} {}".format(s, max(result)))
+    
